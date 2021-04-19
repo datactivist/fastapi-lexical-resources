f21bdfcb3dca5603173d6e3b115ee3cfd6f3fc87
Mathieu Morey <mathieu.morey@gmail.com>
2021-04-19 09:46:49 +0200

    FIX subprocess, pathlib

--- app/preprocess/convert_embeddings.py
+++ app/preprocess/convert_embeddings.py
@@ -2,9 +2,10 @@
 Script to convert every downloaded files from dowload_embeddings.py in the directory tree app/embeddings/* into .magnitude files
 """
 
-import os
 import json
 from pathlib import Path
+from subprocess import call
+
 
 embeddings_metadata_path = Path("../")
 embeddings_path = Path("embeddings")
@@ -14,23 +15,22 @@ def convert_embeddings_to_magnitude(input_file):
     """
     convert the embeddings in parameter in a magnitude file, delete the old embeddings
     """
-
-    input_file = embeddings_path / Path(input_file["type"]) / Path(input_file["name"])
-    output_file = input_file.with_suffix(".magnitude")
+    fp_in = embeddings_path / Path(input_file["type"]) / Path(input_file["name"])
+    output_file = fp_in.with_suffix(".magnitude")
 
     # if magnitude file does not exists already and is activated
-    if not os.path.exists(output_file) and input_file["is_activated"]:
-        print("converting", input_file)
+    if not output_file.exists() and input_file["is_activated"]:
+        print("converting", fp_in)
         command = (
             "python3 -m pymagnitude.converter -i "
-            + str(input_file)
+            + str(fp_in)
             + " -e 'ignore' -o "
             + str(output_file)
         )
-        os.system(command)
-        os.remove(input_file)
+        call(command, shell=True)
+        fp_in.unlink()
     else:
-        print(input_file, "is already converted / is not activated")
+        print(fp_in, "is already converted / is not activated")
 
 
 # Loop on embeddings to convert them
